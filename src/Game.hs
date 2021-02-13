0f13d5ab1c1c42fba345465f2001d33661544a84
Ivy <suguivy@riseup.net>
2021-02-13 21:23:34 +0100

    Fixed a bug

--- src/Game.hs
+++ src/Game.hs
@@ -21,9 +21,9 @@ newGame :: Game
 newGame = Game (makeDungeon 30 10) (Player 0 0)
 
 runAction :: Action -> Game -> Maybe Game
-runAction (Walk N) game = Just $ game & player . y %~ (-)1
-runAction (Walk S) game = Just $ game & player . y %~ (+)1
-runAction (Walk W) game = Just $ game & player . x %~ (-)1
-runAction (Walk E) game = Just $ game & player . x %~ (+)1
+runAction (Walk N) game = Just $ game & player . y %~ (1 `subtract`)
+runAction (Walk S) game = Just $ game & player . y %~ (+1)
+runAction (Walk W) game = Just $ game & player . x %~ (1 `subtract`)
+runAction (Walk E) game = Just $ game & player . x %~ (+1)
 runAction None g = Just g
 runAction ExitGame _ = Nothing
